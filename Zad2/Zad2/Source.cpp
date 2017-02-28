43df86c89bd3bf392b91eb62b4126a432b852682
PetranTinak <anonie@azet.sk>
2017-02-28 17:50:02 +0100

    Rozpoznanie kruhu
    Spustitelný kod

--- Zad2/Zad2/Source.cpp
+++ Zad2/Zad2/Source.cpp
@@ -1,25 +1,48 @@
-#include "opencv2/opencv.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
 
 using namespace cv;
-
-int main(int, char**)
+using namespace std;
+static void help()
 {
-	VideoCapture cap(0); // open the default camera
-	if (!cap.isOpened())  // check if we succeeded
-		return -1;
-
-	Mat edges;
-	namedWindow("edges", 1);
-	for (;;)
+	cout << "\nThis program demonstrates circle finding with the Hough transform.\n"
+		"Usage:\n"
+		"./houghcircles <image_name>, Default is ../data/board.jpg\n" << endl;
+}
+int main()
+{
+	
+	string filename = "C:/Users/Boi/Documents/Obrazky/image121.bmp";
+	if (filename.empty())
 	{
-		Mat frame;
-		cap >> frame; // get a new frame from camera
-		cvtColor(frame, edges, COLOR_BGR2GRAY);
-		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-		Canny(edges, edges, 0, 30, 3);
-		imshow("edges", edges);
-		if (waitKey(30) >= 0) break;
+		help();
+		cout << "no image_name provided" << endl;
+		return -1;
 	}
-	// the camera will be deinitialized automatically in VideoCapture destructor
+	Mat img = imread(filename, 0);
+	if (img.empty())
+	{
+		help();
+		cout << "can not open " << filename << endl;
+		return -1;
+	}
+	Mat cimg;
+	medianBlur(img, img, 5);
+	cvtColor(img, cimg, COLOR_GRAY2BGR);
+	vector<Vec3f> circles;
+	HoughCircles(img, circles, HOUGH_GRADIENT, 1, 10,
+		100, 30, 1, 30 // change the last two parameters
+					   // (min_radius & max_radius) to detect larger circles
+	);
+	for (size_t i = 0; i < circles.size(); i++)
+	{
+		Vec3i c = circles[i];
+		circle(cimg, Point(c[0], c[1]), c[2], Scalar(0, 0, 255), 3, LINE_AA);
+		circle(cimg, Point(c[0], c[1]), 2, Scalar(0, 255, 0), 3, LINE_AA);
+	}
+	imshow("detected circles", cimg);
+	waitKey();
 	return 0;
 }
